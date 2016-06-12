5f44924297886da74b8fed0ade6d707c645eb576
François SAMIN <francois.samin@gmail.com>
2016-06-12 17:35:33 +0200

    doc

--- package.go
+++ package.go
@@ -1,1 +1,3 @@
+//Package wsqueue provides a framework over gorilla/mux and gorilla/websocket to operate kind of AMQP but over websockets
+//It offers a Server, a Client and two king of messaging protocols : Topic and Queue
 package wsqueue
